6b7a758866495791f370eee95b5440df60a26ec2
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-03 22:19:42 +0900

    UIの設定とpod installやり直し

---
+++ Swift5IntroApp1/Controller/IntroViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IntroViewController.swift
+//  Swift5IntroApp1
+//
+//  Created by Takuya Ando on 2021/01/03.
+//
+
+import UIKit
+import Lottie
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
